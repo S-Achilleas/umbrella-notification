95696fd3448ac484326c8613b0a63c0b5ec9f9e4
Achilleas <sarraxilleas@gmail.com>
2024-10-13 15:16:26 +0300

    changed the script to have an OOP approach and testing ways to get the hourly temperature

--- web.py
+++ web.py
@@ -3,19 +3,21 @@ from bs4 import BeautifulSoup
 
 class Hourly_Weather:
     def __init__(self,html_content):
-        this.html_content = html_content
+        self.html_content = html_content
     
     def get_temperature(self):
-        temp = self.split('°')
+        temp = self.html_content.split('°')
+        return temp[0]
 
 page = requests.get("https://weather.com/el-GR/weather/today/l/GRXX0004:1:GR")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-todays_weather = soup.find('div', class_="TodayWeatherCard--TableWrapper--wt2ea")
-li_elements = soup.find_all('li',class_ =["Column--column--gUiRn Column--verticalStack--k9S2a","Column--column--gUiRn Column--active--OWlqB Column--verticalStack--k9S2a"])
-counter = 0 
-for li in li_elements:
-    counter += 1
-    if counter > 4:
-        break
-    print(li.text)
+
+def get_todays_weather():
+    todays_weather = soup.find('div', class_="TodayWeatherCard--TableWrapper--wt2ea")
+    li_elements = todays_weather.find_all('li',class_ =["Column--column--gUiRn Column--verticalStack--k9S2a","Column--column--gUiRn Column--active--OWlqB Column--verticalStack--k9S2a"])
+    for li in li_elements:
+        weather = Hourly_Weather(li.text)
+        print("Temperature: ",weather.get_temperature())
+
+get_todays_weather()
