35eb1d216a8225f2b9d51e20eb45ad7f458cdb94
Achilleas <sarraxilleas@gmail.com>
2024-10-13 15:27:41 +0300

    converted the hourly_weather class to get the propability to rain each day

--- web.py
+++ web.py
@@ -5,9 +5,9 @@ class Hourly_Weather:
     def __init__(self,html_content):
         self.html_content = html_content
     
-    def get_temperature(self):
-        temp = self.html_content.split('°')
-        return temp[0]
+    def get_rain(self):
+        temp = self.html_content.split()
+        return temp[-1][-2]
 
 page = requests.get("https://weather.com/el-GR/weather/today/l/GRXX0004:1:GR")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,6 +18,11 @@ def get_todays_weather():
     li_elements = todays_weather.find_all('li')
     for li in li_elements:
         weather = Hourly_Weather(li.text)
-        print("Temperature: ",weather.get_temperature())
+        print("Rain: ",weather.get_rain())
 
 get_todays_weather()
+
+'''Πρωί23°Partly Cloudy--
+Απόγευμα26°Mostly CloudyRainΠιθανότητα βροχής1%
+Βράδυ21°Clear NightRainΠιθανότητα βροχής2%
+Νύχτα18°Clear NightRainΠιθανότητα βροχής4%'''
