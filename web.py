9b9a5f68e79e7d3f5edf5714048a6911ac2040d0
Achilleas <sarraxilleas@gmail.com>
2024-10-13 15:01:43 +0300

    narrowed the results of the html content in desired results

--- web.py
+++ web.py
@@ -5,5 +5,10 @@ page = requests.get("https://weather.com/el-GR/weather/today/l/GRXX0004:1:GR")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 todays_weather = soup.find('div', class_="TodayWeatherCard--TableWrapper--wt2ea")
-list_of_weather = soup.find_all('li')
-print(len(list_of_weather))
+li_elements = soup.find_all('li',class_ =["Column--column--gUiRn Column--verticalStack--k9S2a","Column--column--gUiRn Column--active--OWlqB Column--verticalStack--k9S2a"])
+counter = 0 
+for li in li_elements:
+    counter += 1
+    if counter > 4:
+        break
+    print(li.text)
