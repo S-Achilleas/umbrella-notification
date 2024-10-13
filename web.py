ab72d1f123486e813b84a962a0f8a73e00df089c
Achilleas <sarraxilleas@gmail.com>
2024-10-13 14:46:39 +0300

    solved the blank list issue

--- web.py
+++ web.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 page = requests.get("https://weather.com/el-GR/weather/today/l/GRXX0004:1:GR")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-todays_weather = soup.find_all('div', class_='"TodayWeatherCard--TableWrapper--wt2ea"')
+todays_weather = soup.find('div', class_="TodayWeatherCard--TableWrapper--wt2ea")
+list_of_weather = soup.find_all('li')
+print(len(list_of_weather))
